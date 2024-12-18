4c86bd3cd63c17f53c3808ad35ceb2a7800476ef
syam <n.syamprasad@gmail.com>
2024-12-18 22:57:40 +0530

    some init msg

---
+++ src/test/java/rahulshettyacadamy/Day1.java
@@ -0,0 +1,20 @@
+package rahulshettyacadamy;
+
+import org.testng.annotations.Test;
+
+public class Day1 {
+
+	@Test()
+	public void HomeLoan1()
+	{
+		System.out.println("HomeLoan1");
+
+	}
+	@Test()
+	public void HomeLoan2()
+	{
+		System.out.println("HomeLoan2");
+
+	}
+
+}
---
+++ src/test/java/rahulshettyacadamy/Day2.java
@@ -0,0 +1,20 @@
+package rahulshettyacadamy;
+
+import org.testng.annotations.Test;
+
+public class Day2 {
+
+	@Test()
+	public void PersonalLoan1()
+	{
+		System.out.println("PersonalLoan1");
+
+	}
+	@Test()
+	public void PersonalLoan2()
+	{
+		System.out.println("PersonalLoan2");
+
+	}
+
+}
